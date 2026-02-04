ba58253a3a8ebd296fcd944281ada82342e0b59f
Ben Joe Mathews <joemathews4development@gmail.com>
2026-02-04 15:45:27 +0100

    completed the first bonus

--- src/components/travelList.jsx
+++ src/components/travelList.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import data from "../assets/travel-plans.json"
+import TravelPlanCard from "./travelPlanCard"
 
 function TravelList() {
 
     const [travelList, setTravelList] = useState(data)
 
-    const premiumLabelStyle = {color: "white", padding: "5px", backgroundColor: "#366ACA", borderRadius: "5px"}
+    
 
     const deleteTravelPlan = (removeId) => {
         setTravelList((previous) => {
@@ -17,21 +18,7 @@ function TravelList() {
         <div>
             {travelList.map((travel) => {
                 return(
-                    <div style={{padding: "20px", borderWidth: "1px", borderStyle: "solid", borderColor: "black", display: "flex", gap: "20px", margin: "10px"}}>
-                        <img style={{height: "250px", width: "350px"}} src={travel.image} alt="place" />
-                        <div style={{textAlign: "left"}}>
-                            <h3>{travel.destination} ({travel.days} Days)</h3>
-                            <p style={{fontStyle:"italic"}}>{travel.description}</p>
-                            <p><span style={{fontWeight:"bold"}}>Price: </span>{travel.totalCost} €</p>
-                            <div style={{display:"flex", gap: "10px"}}>
-                                {travel.totalCost <= 350 && <label style={{color: "black", padding: "5px", backgroundColor: "#77BC76", borderRadius: "5px"}}>Great Deal</label>}
-                                {travel.totalCost >= 1500 && <label style={premiumLabelStyle}>Premium</label>}
-                                {travel.allInclusive && <label style={premiumLabelStyle}>All-inclusive</label>}
-                            </div>
-                            <br />
-                            <button style={{color: "black", padding: "5px", backgroundColor: "#ABA9AB", borderRadius: "5px"}} onClick={() => deleteTravelPlan(travel.id)}>Delete</button>
-                        </div>
-                    </div>
+                    <TravelPlanCard key={travel.id} plan={travel} onDelete={deleteTravelPlan} />
                 )
             })}
         </div>
---
+++ src/components/travelPlanCard.jsx
@@ -0,0 +1,24 @@
+function TravelPlanCard(props) {
+    
+    const premiumLabelStyle = {color: "white", padding: "5px", backgroundColor: "#366ACA", borderRadius: "5px"}
+    return (
+        <div style={{ padding: "20px", borderWidth: "1px", borderStyle: "solid", borderColor: "black", display: "flex", gap: "20px", margin: "10px" }}>
+            <img style={{ height: "250px", width: "350px" }} src={props.plan.image} alt="place" />
+            <div style={{ textAlign: "left" }}>
+                <h3>{props.plan.destination} ({props.plan.days} Days)</h3>
+                <p style={{ fontStyle: "italic" }}>{props.plan.description}</p>
+                <p><span style={{ fontWeight: "bold" }}>Price: </span>{props.plan.totalCost} €</p>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    {props.plan.totalCost <= 350 && <label style={{ color: "black", padding: "5px", backgroundColor: "#77BC76", borderRadius: "5px" }}>Great Deal</label>}
+                    {props.plan.totalCost >= 1500 && <label style={premiumLabelStyle}>Premium</label>}
+                    {props.plan.allInclusive && <label style={premiumLabelStyle}>All-inclusive</label>}
+                </div>
+                <br />
+                <button style={{ color: "black", padding: "5px", backgroundColor: "#ABA9AB", borderRadius: "5px" }} onClick={() => props.onDelete(props.plan.id)}>Delete</button>
+            </div>
+        </div>
+    )
+    
+}
+
+export default TravelPlanCard
